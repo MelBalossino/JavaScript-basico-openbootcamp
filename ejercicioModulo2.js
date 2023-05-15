ffdca9cc633ce1027c5e6273b0ba4a31751a0532
Melodi Balossino <124601449+MelBalossino@users.noreply.github.com>
2023-05-15 18:14:31 -0300

    Add files via upload

---
+++ ejercicioModulo2.js
@@ -0,0 +1,30 @@
+// EJERCICIO MÓDULO 2
+/* Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string): Melodi
+
+- Tu edad (number): 32
+
+- ¿Eres desarrollador? (boolean): true
+
+- Tu fecha de nacimiento (Date): 14/08/1990
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/
+const libroFav = {
+    titulo: "El poder del ahora",
+    autor: "Eckhart Tolle",
+    fecha: 1997,
+    url: "https://es.wikipedia.org/wiki/El_poder_del_Ahora"
+}
+
+const listaAboutMe = ["Melodi", 32, true, new Date(1990, 7, 14), libroFav];
+console.log(listaAboutMe);
+
+
+
+
+
+
+
+
