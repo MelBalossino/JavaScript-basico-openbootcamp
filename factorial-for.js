d9276be2e13ecda0b5846fd749b6787bffb4441a
Melodi Balossino <124601449+MelBalossino@users.noreply.github.com>
2023-05-16 14:29:34 -0300

    Add files via upload

---
+++ factorial-for.js
@@ -0,0 +1,13 @@
+/* EJERCICIOS MÓDULO 3 
+Crea el siguiente archivo JS:
+
+- factorial-for.js -> Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+
+*/
+
+// BUCLE FOR
+let factorial = 1;
+for (let i = 1; i < 11; i++) {
+    factorial = factorial * i;
+    console.log (factorial);
+}
