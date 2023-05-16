f584764acdbfe94acf74ce6359bc66033fa8405f
Melodi Balossino <124601449+MelBalossino@users.noreply.github.com>
2023-05-16 14:43:37 -0300

    Add files via upload

---
+++ factorial-while.js
@@ -0,0 +1,14 @@
+/* EJERCICIO MODULO 3 PARTE 2
+Crear el siguiente archivo:
+
+* factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while
+
+*/
+
+let i = 1;
+let factorial = 1;
+while (i < 11) {
+   factorial = factorial * i;
+    i++;
+}
+
