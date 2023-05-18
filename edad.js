86801613a9bcff7a6bf5d33939ab0aba7e3877bb
Melodi Balossino <124601449+MelBalossino@users.noreply.github.com>
2023-05-18 18:01:12 -0300

    Add files via upload

---
+++ edad.js
@@ -0,0 +1,10 @@
+// EJERCICIOS MÓDULO 7- PARTE 4
+//Abre una nueva ventana "about:blank" en Google Chrome
+//Abre la consola de desarrollador de Google Chrome (F12)
+//Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+
+prompt(“Cuál es tu edad?”);
+let edad = prompt(“Cuál es tu edad?”);
+let numEdad = Number(edad);
+ 
+
